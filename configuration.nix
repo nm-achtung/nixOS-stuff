3e3b1dc2dd4dccb0f8f29fd48c25243588301f00
sheeponmeth2635 <119545319+sheeponmeth2635@users.noreply.github.com>
2023-01-04 16:34:54 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,7 @@
 
 
   home-manager.users.nate = {
-  home.packages = [ pkgs.neofetch pkgs.prboom-plus pkgs.gzdoom ];
+  home.packages = [ pkgs.neofetch pkgs.prboom-plus ];
   programs.bash.enable = true;
   home.stateVersion = "22.11";
   };
